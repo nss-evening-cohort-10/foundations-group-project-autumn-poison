249f7aa550c6c4c2e7699cfbf3be3e4bc1bb560c
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-09-17 20:58:39 -0500

    banner complete

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const images = [{
-  image1 = " C:\Users\John\workspace\projects\foundations-group-project-autumn-poison\images\daft-punk.jpg",
-  image2 = "C:\Users\John\workspace\projects\foundations-group-project-autumn-poison\images\garage-rock.jpg",
-  image3 = "https://upload.wikimedia.org/wikipedia/commons/5/52/Greyskull_-_Texas_Punk_Rock_Band.jpg",
-  image4 = "https://upload.wikimedia.org/wikipedia/commons/b/bb/-Agnostic_Front_live_in_Rome-2.jpg",
-  image5 = "https://www.maxpixel.net/static/photo/1x/Grunge-Design-Dirty-Underground-Music-Punk-Grungy-2891524.jpg",
+  image1: " C:\Users\John\workspace\projects\foundations-group-project-autumn-poison\images\daft-punk.jpg",
+  image2: "C:\Users\John\workspace\projects\foundations-group-project-autumn-poison\images\garage-rock.jpg",
+  image3: "https://upload.wikimedia.org/wikipedia/commons/5/52/Greyskull_-_Texas_Punk_Rock_Band.jpg",
+  image4: "https://upload.wikimedia.org/wikipedia/commons/b/bb/-Agnostic_Front_live_in_Rome-2.jpg",
+  image5: "https://www.maxpixel.net/static/photo/1x/Grunge-Design-Dirty-Underground-Music-Punk-Grungy-2891524.jpg",
 }];
 
 
