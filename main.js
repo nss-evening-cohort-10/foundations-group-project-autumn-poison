788ee526418e6080c49b8d3e08eaf99cbd529993
Jamie <phillipsja97@gmail.com>
2019-09-17 20:50:43 -0500

    finished product cards

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const products = [
   { name: '"Songs of Anger, Songs of Hope" Cassette Tape',
     type: 'cassettes',
     imageUrl: 'https://2.bp.blogspot.com/-xGDuj4duODA/Wz9rk0bY4CI/AAAAAAAAOGA/0waWkrAh3NYbv7rJrc30sJ_dccUIk2MiACLcBGAs/s1600/R-5719091-1407495216-6363.jpeg.jpg',
-    description: 'Buy our 2nd full cassette "Songs of Anger, Songs of Hope" with hits like: "Worship the Bomb", "Who Would Rape Mother Earth", and "Dirty Business!"',
+    description: 'Buy our 2nd full cassette "Songs of Anger, Songs of Hope" with hits like: "Worship the Bomb", and "Dirty Business!"',
   },
  
   { name: 'Kitchen Sink Politics Cassette Tape',
@@ -19,25 +19,25 @@ const products = [
   { name: '"Bullshit Detector Volume 4" Cassette Tape',
     type: 'cassettes',
    imageUrl: 'https://e.snmc.io/i/300/w/2c244d12783e26b840769e223446672a/3460362',
-   description: 'Buy the latest album that we are featured on called "Bullshit Detector Volume 4" with our track labeled: "Utopia: A New World in Our Hearts."',
+   description: 'Heres the latest compilation album we are on called "Bullshit Detector Volume 4" with our track: "Utopia - A New World in Our Hearts."',
   },
  
-  { name: 'Concert Poster',
+  { name: 'Concert Poster from our First Ever Show',
     type: 'poster',
     imageUrl: 'http://i192.photobucket.com/albums/z149/pengy1966/pengy1966%20stuff/KYPP435.jpg',
-    description: 'Take home a souvenir from the largest concert we have played to date!',
+    description: 'Take home a souvenir poster from the largest concert we have played to date with Kronstadt Rising and The Poets! ',
   },
  
   { name: 'First ever Autumn Poison Concert Poster',
     type: 'poster',
    imageUrl: 'http://i192.photobucket.com/albums/z149/pengy1966/KYPP838.jpg',
-   description: 'Put our poster on your wall of our first ever gig! The legends were born that night.',
+   description: 'Put our poster on your wall of our first ever gig with Sheena and Paul rocking out! The legends were born that night.',
   },
  
-  { name: 'Punk Rock Poster',
+  { name: 'Punk Rock Poster proving its all about emotion',
     type: 'poster',
    imageUrl: 'http://www.southendpunk.com/images/thebands/autumn3l.jpg',
-   description: 'Let the emotion overcome you! Here’s a poster that reminds us that it’s all about the music and the message.',
+   description: 'Let the emotion overcome you, Julian! Here’s a poster that reminds us that it’s all about the music and the message.',
   }];
 
 
@@ -46,11 +46,15 @@ const products = [
       for (let i = 0; i < productsArray.length; i++) {
         let product = productsArray[i];
         domString += `
+        <div class="individualProductCard">
         <div class="product-card text-center">
         <h1 class="productCardTitle">${product.name}</h1>
         <img class="productImage" src=${product.imageUrl} alt='image of ${product.name}'/>
-        <h3>${product.description}</h3>
+        <h4 class"productCardBody">${product.description}</h4>
+        <footer>
         <button type="button" class="btn btn-primary">Buy</button>
+        </footer>
+        </div>
         </div>
         `
         };
