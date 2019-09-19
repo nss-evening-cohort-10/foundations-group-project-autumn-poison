04f9296700ad6d5d753abe654b77d162ce8707bf
Jamie <phillipsja97@gmail.com>
2019-09-19 00:01:16 -0500

    buy Buttons work

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const printToDOM = (textToPrint, divID) => {
+  document.getElementById(divID).innerHTML = textToPrint;
+}; 
+
 const images = [
   "images/daft-punk.jpg",
 
@@ -38,14 +42,7 @@ function setImage(image) {
   document.querySelectorAll(".slide")[0].src = images[image];
 }
 
-//window.onload = initSlideshow();
-
-
-
-
-const printToDOM = (textToPrint, divID) => {
-  document.getElementById(divID).innerHTML = textToPrint;
-}; 
+// window.onload = initSlideshow();
 
 
 //Array of Upcoming Concert Details
@@ -94,69 +91,59 @@ const tourDates = [
     }
 ];
 
-
-const printToDOM = (textToPrint, divID) => {
-    document.getElementById(divID).innerHTML = textToPrint;
-  }  
-
   //Function that filters info from Upcoming Concerts Array and prints into collapsable list
-const eventDetails = (eventsArray) => {
-    let eventDomstring = ''
-    for (i=0; i < eventsArray.length; i++) {
-        let event = eventsArray[i]
-        eventDomstring += `
-        <div class="accordion" id="accordionExample">
-            <div class="card text-center">
-                <div class="card-header" id="headingOne">
-                    <h2 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseOne${i}" aria-expanded="false" aria-controls="collapseOne">
-                    ${event.city}, ${event.date}
-                    </button>
-                    </h2>
-                </div>
 
-                <div id="collapseOne${i}" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
-                     <div class="card-body">
-                        <h3>${event.venue}</h3>
-                        <h3>${event.time}</h3>
-                        <h3>${event.price}</h3>
-                        <a>Button Placeholder</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `
-    }
-    printToDOM(eventDomstring, 'concert-zone')
-}
-
-eventDetails(tourDates);
-
-const images = [{
-  image1: " C:\Users\John\workspace\projects\foundations-group-project-autumn-poison\images\daft-punk.jpg",
-  image2: "C:\Users\John\workspace\projects\foundations-group-project-autumn-poison\images\garage-rock.jpg",
-  image3: "https://upload.wikimedia.org/wikipedia/commons/5/52/Greyskull_-_Texas_Punk_Rock_Band.jpg",
-  image4: "https://upload.wikimedia.org/wikipedia/commons/b/bb/-Agnostic_Front_live_in_Rome-2.jpg",
-  image5: "https://www.maxpixel.net/static/photo/1x/Grunge-Design-Dirty-Underground-Music-Punk-Grungy-2891524.jpg",
-}];
-
-const albums =  [
-  {
-  imageUrl: 'https://i.pinimg.com/originals/c8/39/3c/c8393cc3bbf105906ef2a93f22947863.jpg',
-  title: 'Songs of Anger, Songs of Hope ',
-  songlist: [
-     'Now That Is What I Call Edgy',
-     'Hiroshima',
-     'War Crimes (Afghanistan, El Salvador)',
-     'Uranium',
-     'Smokescreen',
-     'Animals Are Not Ours...',
-     'Contributory Negligence',
-     'Dirty Business',
-     'Porton Down',
-     'Sense Your Own Strength'
-     ]
-
+   
+     const albums =  [
+       {
+         imageUrl: 'https://i.pinimg.com/originals/c8/39/3c/c8393cc3bbf105906ef2a93f22947863.jpg',
+        title: 'Songs of Anger, Songs of Hope ',
+        songlist: [
+       'Now That Is What I Call Edgy',
+       'Hiroshima',
+       'War Crimes (Afghanistan, El Salvador)',
+       'Uranium',
+       'Smokescreen',
+       'Animals Are Not Ours...',
+       'Contributory Negligence',
+       'Dirty Business',
+       'Porton Down',
+      'Sense Your Own Strength'
+       ]
+      },
+      {
+    imageUrl: 'https://cdn.shopify.com/s/files/1/1365/2497/products/Rainbow-unicorn-mask_900x.png?v=1553633323',
+    title: 'Kitchen Sink Politics',
+    songlist: [
+      'React is Around The Corner',
+        'Do You Even PrintToDom?',
+        'Song of the Experts',
+        'A Message To All Pielords',
+        'Stifled Colors',
+        'Superiority Hypocrisy',
+        'Thot Army',
+        'A National Anthem',
+        'Kitchen Sink Politics',
+        'Utopia: (A New World In Our Hearts)'
+      ]
+    },
+    {
+      imageUrl: 'https://d3avoj45mekucs.cloudfront.net/rojakdaily/media/iylia/lifestyle/pie2.png',
+      title: 'Bulls**t Detector Volume 4',
+      songlist: [
+        'Pie Is Love, Pie Is Life',
+        'One Thousand Pies',
+        'Libation Pie',
+        'Half The Pie',
+        'What The Pie Says Goes',
+        'Swords Into Pies',
+        'Worship The Pie',
+        'The Pie And The Glory',
+        'Pie Hunting',
+        'The Pie Says',
+      ]
+    },
+  ];
 
 
 const products = [
@@ -165,35 +152,35 @@ const products = [
     imageUrl: 'https://2.bp.blogspot.com/-xGDuj4duODA/Wz9rk0bY4CI/AAAAAAAAOGA/0waWkrAh3NYbv7rJrc30sJ_dccUIk2MiACLcBGAs/s1600/R-5719091-1407495216-6363.jpeg.jpg',
     description: 'Buy our 2nd full cassette "Songs of Anger, Songs of Hope" with hits like: "Worship the Bomb", and "Dirty Business!"',
   },
- 
+
   { name: 'Kitchen Sink Politics Cassette Tape',
     type: 'cassettes',
     imageUrl:'http://i192.photobucket.com/albums/z149/pengy1966/scan522.jpg',
     description: 'Buy your 1st full cassette tape "Kitchen Sink Politics" with hits like: "Joke Army", "Half the Sky" and more!',
   },
- 
+
   { name: '"Bulls**t Detector Volume 4" Cassette Tape',
     type: 'cassettes',
-   imageUrl: 'https://e.snmc.io/i/300/w/2c244d12783e26b840769e223446672a/3460362',
-   description: 'Heres a compilation album we are on called "Bulls**t Detector Volume 4" with our track: "Utopia - A New World"',
+  imageUrl: 'https://e.snmc.io/i/300/w/2c244d12783e26b840769e223446672a/3460362',
+  description: 'Heres a compilation album we are on called "Bulls**t Detector Volume 4" with our track: "Utopia - A New World"',
   },
- 
+
   { name: 'Concert Poster from our First Ever Show',
     type: 'poster',
     imageUrl: 'http://i192.photobucket.com/albums/z149/pengy1966/pengy1966%20stuff/KYPP435.jpg',
     description: 'Take home a souvenir poster from the largest concert we have played to date with Kronstadt Rising and The Poets! ',
   },
- 
+
   { name: 'First ever Autumn Poison Concert Poster',
     type: 'poster',
-   imageUrl: 'http://i192.photobucket.com/albums/z149/pengy1966/KYPP838.jpg',
-   description: 'Put our poster on your wall of our first ever gig with Sheena and Paul rocking out! The legends were born that night.',
+  imageUrl: 'http://i192.photobucket.com/albums/z149/pengy1966/KYPP838.jpg',
+  description: 'Put our poster on your wall of our first ever gig with Sheena and Paul rocking out! The legends were born that night.',
   },
- 
+
   { name: 'Punk Rock Poster proving its all about emotion',
     type: 'poster',
-   imageUrl: 'http://www.southendpunk.com/images/thebands/autumn3l.jpg',
-   description: 'Let the emotion overcome you, Julian! Here’s a poster that reminds us that it’s all about the music and the message.',
+  imageUrl: 'http://www.southendpunk.com/images/thebands/autumn3l.jpg',
+  description: 'Let the emotion overcome you, Julian! Here’s a poster that reminds us that it’s all about the music and the message.',
   },
 
   { name: 'A Real Punk Rock Sticker',
@@ -214,59 +201,7 @@ const products = [
     imageUrl: 'https://cdn.shopify.com/s/files/1/1506/9366/products/DSC00029_1024x1024.JPG?v=1527326481',
     description: 'Fight the system. Destroy the power. ',
   },
-
 ];
-
-   const albums =  [
-    {
-    imageUrl: 'https://i.pinimg.com/originals/c8/39/3c/c8393cc3bbf105906ef2a93f22947863.jpg',
-    title: 'Songs of Anger, Songs of Hope ',
-    songlist: [
-       'Now That Is What I Call Edgy',
-       'Hiroshima',
-       'War Crimes (Afghanistan, El Salvador)',
-       'Uranium',
-       'Smokescreen',
-       'Animals Are Not Ours...',
-       'Contributory Negligence',
-       'Dirty Business',
-       'Porton Down',
-       'Sense Your Own Strength'
-       ]
-    },
-    {
-    imageUrl: 'https://cdn.shopify.com/s/files/1/1365/2497/products/Rainbow-unicorn-mask_900x.png?v=1553633323',
-    title: 'Kitchen Sink Politics',
-    songlist: [
-        'React is Around The Corner',
-        'Do You Even PrintToDom?',
-        'Song of the Experts',
-        'A Message To All Pielords',
-        'Stifled Colors',
-        'Superiority Hypocrisy',
-        'Thot Army',
-        'A National Anthem',
-        'Kitchen Sink Politics',
-        'Utopia: (A New World In Our Hearts)'
-       ]
-    },
-    {
-    imageUrl: 'https://d3avoj45mekucs.cloudfront.net/rojakdaily/media/iylia/lifestyle/pie2.png',
-    title: 'Bulls**t Detector Volume 4',
-    songlist: [
-        'Pie Is Love, Pie Is Life',
-        'One Thousand Pies',
-        'Libation Pie',
-        'Half The Pie',
-        'What The Pie Says Goes',
-        'Swords Into Pies',
-        'Worship The Pie',
-        'The Pie And The Glory',
-        'Pie Hunting',
-        'The Pie Says',
-      ]
-    },
-    ];
 
 // function to print the different merch products to the merch page.
   const productBuilder = (productsArray) => {
@@ -280,7 +215,7 @@ const products = [
             <img class="productImage" src=${product.imageUrl} alt='image of ${product.name}'/>
              <h5 class"productCardBody">${product.description}</h4>
               <footer>
-               <button type="button" id="buy" class="btn btn-primary">Buy</button>
+              <a href="purchase.html" class="btn btn-primary">Buy</a>
               </footer>
           </div>
          </div>
@@ -334,7 +269,7 @@ const products = [
                       <h3>${event.venue}</h3>
                       <h3>${event.time}</h3>
                       <h3>${event.price}</h3>
-                      <a>Button Placeholder</a>
+                      <a href="purchase.html" class="btn btn-primary">Buy</a>
                   </div>
               </div>
           </div>
@@ -345,6 +280,8 @@ const products = [
 };
 
 // eventDetails(tourDates);
+
+ 
 // init function will allow us to run each function on it's specific page.
 const init = () => {
   if (document.URL.includes('disc')) {
@@ -379,13 +316,7 @@ const sortClick = (event) => {
     };
 };
 
-document.getElementById('product-card').addEventListener('click', function(event) {
-  if (event.target.id === 'buy' ) {
-      console.log(event.target.id);
-      window.location.href='./purchase.html';
-  }
-}
-);
+
 
 document.getElementById('all').addEventListener('click', sortClick);
 document.getElementById('cassettes').addEventListener('click', sortClick);
