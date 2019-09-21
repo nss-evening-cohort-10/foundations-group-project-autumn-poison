4d67aaa0af10222beb58e18dcaa2db4a0a6cd4e7
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-09-21 08:37:46 -0500

    made code prettier

--- main.js
+++ main.js
@@ -1,185 +1,208 @@
 const printToDOM = (textToPrint, divID) => {
   document.getElementById(divID).innerHTML = textToPrint;
-}; 
-
+};
 
 //Array of Upcoming Concert Details
 const tourDates = [
-    {
-        city:'Nashville',
-        venue: 'Exit/In',
-        price: '$15',
-        date: '04/20',
-        time: '10pm'
-    },
-    {
-        city:'Chicago',
-        venue: 'Bar Bar Black Sheep',
-        price: '$10',
-        date: '04/22',
-        time: '10pm'
-    },
-    {
-        city:'Duluth',
-        venue: 'Cheese World',
-        price: '$20',
-        date: '04/25',
-        time: '8pm'
-    },
-    {
-        city:'Boise',
-        venue: 'The Golden Potato',
-        price: '$5',
-        date: '06/09',
-        time: '10pm'
-    },
-    {
-        city:'Portland',
-        venue: 'Tanis',
-        price: '$25',
-        date: '07/07',
-        time: '10pm'
-    },
-    {
-        city:'Austin',
-        venue: 'The Chain Drive',
-        date: '06/09',
-        price: '$10',
-        time: '10pm'
-    }
+  {
+    city: "Nashville",
+    venue: "Exit/In",
+    price: "$15",
+    date: "04/20",
+    time: "10pm"
+  },
+  {
+    city: "Chicago",
+    venue: "Bar Bar Black Sheep",
+    price: "$10",
+    date: "04/22",
+    time: "10pm"
+  },
+  {
+    city: "Duluth",
+    venue: "Cheese World",
+    price: "$20",
+    date: "04/25",
+    time: "8pm"
+  },
+  {
+    city: "Boise",
+    venue: "The Golden Potato",
+    price: "$5",
+    date: "06/09",
+    time: "10pm"
+  },
+  {
+    city: "Portland",
+    venue: "Tanis",
+    price: "$25",
+    date: "07/07",
+    time: "10pm"
+  },
+  {
+    city: "Austin",
+    venue: "The Chain Drive",
+    date: "06/09",
+    price: "$10",
+    time: "10pm"
+  }
 ];
 
-const images = [{
-   imageUrl: "https://upload.wikimedia.org/wikipedia/commons/5/52/Greyskull_-_Texas_Punk_Rock_Band.jpg"
-}];
+const images = [
+  {
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/5/52/Greyskull_-_Texas_Punk_Rock_Band.jpg"
+  }
+];
 
-const imageBuilder = (imageArray) => {
+const imageBuilder = imageArray => {
   let imageDomString = "";
   for (let i = 0; i < imageArray.length; i++) {
-   let image = imageArray[i];
-    imageDomString += `<img src='${image.imageUrl}'>`
+    let image = imageArray[i];
+    imageDomString += `<img src='${image.imageUrl}'>`;
   }
-    printToDOM(imageDomString, 'photo');     
-}; 
+  printToDOM(imageDomString, "photo");
+};
 
-    
-    //imageBuilder(images);
+//imageBuilder(images);
 
 const products = [
-  { name: '"Songs of Anger, Songs of Hope" Cassette Tape',
-    type: 'cassettes',
-    imageUrl: 'https://2.bp.blogspot.com/-xGDuj4duODA/Wz9rk0bY4CI/AAAAAAAAOGA/0waWkrAh3NYbv7rJrc30sJ_dccUIk2MiACLcBGAs/s1600/R-5719091-1407495216-6363.jpeg.jpg',
-    description: 'Buy our 2nd full cassette "Songs of Anger, Songs of Hope" with hits like: "Worship the Bomb", and "Dirty Business!"',
-  },
- 
-  { name: 'Kitchen Sink Politics Cassette Tape',
-    type: 'cassettes',
-    imageUrl:'http://i192.photobucket.com/albums/z149/pengy1966/scan522.jpg',
-    description: 'Buy your 1st full cassette tape "Kitchen Sink Politics" with hits like: "Joke Army", "Half the Sky" and more!',
-  },
- 
-  { name: '"Bulls**t Detector Volume 4" Cassette Tape',
-    type: 'cassettes',
-   imageUrl: 'https://e.snmc.io/i/300/w/2c244d12783e26b840769e223446672a/3460362',
-   description: 'Heres a compilation album we are on called "Bulls**t Detector Volume 4" with our track: "Utopia - A New World"',
-  },
- 
-  { name: 'Concert Poster from our First Ever Show',
-    type: 'poster',
-    imageUrl: 'http://i192.photobucket.com/albums/z149/pengy1966/pengy1966%20stuff/KYPP435.jpg',
-    description: 'Take home a souvenir poster from the largest concert we have played to date with Kronstadt Rising and The Poets! ',
-  },
- 
-  { name: 'First ever Autumn Poison Concert Poster',
-    type: 'poster',
-   imageUrl: 'http://i192.photobucket.com/albums/z149/pengy1966/KYPP838.jpg',
-   description: 'Put our poster on your wall of our first ever gig with Sheena and Paul rocking out! The legends were born that night.',
-  },
- 
-  { name: 'Punk Rock Poster proving its all about emotion',
-    type: 'poster',
-   imageUrl: 'http://www.southendpunk.com/images/thebands/autumn3l.jpg',
-   description: 'Let the emotion overcome you, Julian! Here’s a poster that reminds us that it’s all about the music and the message.',
+  {
+    name: '"Songs of Anger, Songs of Hope" Cassette Tape',
+    type: "cassettes",
+    imageUrl:
+      "https://2.bp.blogspot.com/-xGDuj4duODA/Wz9rk0bY4CI/AAAAAAAAOGA/0waWkrAh3NYbv7rJrc30sJ_dccUIk2MiACLcBGAs/s1600/R-5719091-1407495216-6363.jpeg.jpg",
+    description:
+      'Buy our 2nd full cassette "Songs of Anger, Songs of Hope" with hits like: "Worship the Bomb", and "Dirty Business!"'
   },
 
-  { name: 'A Real Punk Rock Sticker',
-    type: 'sticker',
-    imageUrl: 'https://res.cloudinary.com/teepublic/image/private/s--HNTufKxp--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_191919,e_outline:48/co_191919,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1514826476/production/designs/2243845_1.jpg',
-    description: 'Are you really Punk Rock? Get a sticker.'
-
+  {
+    name: "Kitchen Sink Politics Cassette Tape",
+    type: "cassettes",
+    imageUrl: "http://i192.photobucket.com/albums/z149/pengy1966/scan522.jpg",
+    description:
+      'Buy your 1st full cassette tape "Kitchen Sink Politics" with hits like: "Joke Army", "Half the Sky" and more!'
   },
 
-  { name: 'Distressed Anarchy Sticker',
-    type: 'sticker',
-    imageUrl: 'https://res.cloudinary.com/teepublic/image/private/s--9Q71W9wb--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_191919,e_outline:48/co_191919,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1516240865/production/designs/2287723_0.jpg',
-    description: 'Let anarchy rain.',
+  {
+    name: '"Bulls**t Detector Volume 4" Cassette Tape',
+    type: "cassettes",
+    imageUrl:
+      "https://e.snmc.io/i/300/w/2c244d12783e26b840769e223446672a/3460362",
+    description:
+      'Heres a compilation album we are on called "Bulls**t Detector Volume 4" with our track: "Utopia - A New World"'
   },
 
-  { name: 'Fight the Power Stickers',
-    type: 'sticker',
-    imageUrl: 'https://cdn.shopify.com/s/files/1/1506/9366/products/DSC00029_1024x1024.JPG?v=1527326481',
-    description: 'Fight the system. Destroy the power. ',
+  {
+    name: "Concert Poster from our First Ever Show",
+    type: "poster",
+    imageUrl:
+      "http://i192.photobucket.com/albums/z149/pengy1966/pengy1966%20stuff/KYPP435.jpg",
+    description:
+      "Take home a souvenir poster from the largest concert we have played to date with Kronstadt Rising and The Poets! "
   },
 
+  {
+    name: "First ever Autumn Poison Concert Poster",
+    type: "poster",
+    imageUrl: "http://i192.photobucket.com/albums/z149/pengy1966/KYPP838.jpg",
+    description:
+      "Put our poster on your wall of our first ever gig with Sheena and Paul rocking out! The legends were born that night."
+  },
+
+  {
+    name: "Punk Rock Poster proving its all about emotion",
+    type: "poster",
+    imageUrl: "http://www.southendpunk.com/images/thebands/autumn3l.jpg",
+    description:
+      "Let the emotion overcome you, Julian! Here’s a poster that reminds us that it’s all about the music and the message."
+  },
+
+  {
+    name: "A Real Punk Rock Sticker",
+    type: "sticker",
+    imageUrl:
+      "https://res.cloudinary.com/teepublic/image/private/s--HNTufKxp--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_191919,e_outline:48/co_191919,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1514826476/production/designs/2243845_1.jpg",
+    description: "Are you really Punk Rock? Get a sticker."
+  },
+
+  {
+    name: "Distressed Anarchy Sticker",
+    type: "sticker",
+    imageUrl:
+      "https://res.cloudinary.com/teepublic/image/private/s--9Q71W9wb--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_191919,e_outline:48/co_191919,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1516240865/production/designs/2287723_0.jpg",
+    description: "Let anarchy rain."
+  },
+
+  {
+    name: "Fight the Power Stickers",
+    type: "sticker",
+    imageUrl:
+      "https://cdn.shopify.com/s/files/1/1506/9366/products/DSC00029_1024x1024.JPG?v=1527326481",
+    description: "Fight the system. Destroy the power. "
+  }
 ];
 
-   const albums =  [
-    {
-    imageUrl: 'https://i.pinimg.com/originals/c8/39/3c/c8393cc3bbf105906ef2a93f22947863.jpg',
-    title: 'Songs of Anger, Songs of Hope ',
+const albums = [
+  {
+    imageUrl:
+      "https://i.pinimg.com/originals/c8/39/3c/c8393cc3bbf105906ef2a93f22947863.jpg",
+    title: "Songs of Anger, Songs of Hope ",
     songlist: [
-       'Now That Is What I Call Edgy',
-       'Hiroshima',
-       'War Crimes (Afghanistan, El Salvador)',
-       'Uranium',
-       'Smokescreen',
-       'Animals Are Not Ours...',
-       'Contributory Negligence',
-       'Dirty Business',
-       'Porton Down',
-       'Sense Your Own Strength'
-       ]
-    },
-    {
-    imageUrl: 'https://cdn.shopify.com/s/files/1/1365/2497/products/Rainbow-unicorn-mask_900x.png?v=1553633323',
-    title: 'Kitchen Sink Politics',
+      "Now That Is What I Call Edgy",
+      "Hiroshima",
+      "War Crimes (Afghanistan, El Salvador)",
+      "Uranium",
+      "Smokescreen",
+      "Animals Are Not Ours...",
+      "Contributory Negligence",
+      "Dirty Business",
+      "Porton Down",
+      "Sense Your Own Strength"
+    ]
+  },
+  {
+    imageUrl:
+      "https://cdn.shopify.com/s/files/1/1365/2497/products/Rainbow-unicorn-mask_900x.png?v=1553633323",
+    title: "Kitchen Sink Politics",
     songlist: [
-        'React is Around The Corner',
-        'Do You Even PrintToDom?',
-        'Song of the Experts',
-        'A Message To All Pielords',
-        'Stifled Colors',
-        'Superiority Hypocrisy',
-        'Thot Army',
-        'A National Anthem',
-        'Kitchen Sink Politics',
-        'Utopia: (A New World In Our Hearts)'
-       ]
-    },
-    {
-    imageUrl: 'https://d3avoj45mekucs.cloudfront.net/rojakdaily/media/iylia/lifestyle/pie2.png',
-    title: 'Bulls**t Detector Volume 4',
+      "React is Around The Corner",
+      "Do You Even PrintToDom?",
+      "Song of the Experts",
+      "A Message To All Pielords",
+      "Stifled Colors",
+      "Superiority Hypocrisy",
+      "Thot Army",
+      "A National Anthem",
+      "Kitchen Sink Politics",
+      "Utopia: (A New World In Our Hearts)"
+    ]
+  },
+  {
+    imageUrl:
+      "https://d3avoj45mekucs.cloudfront.net/rojakdaily/media/iylia/lifestyle/pie2.png",
+    title: "Bulls**t Detector Volume 4",
     songlist: [
-        'Pie Is Love, Pie Is Life',
-        'One Thousand Pies',
-        'Libation Pie',
-        'Half The Pie',
-        'What The Pie Says Goes',
-        'Swords Into Pies',
-        'Worship The Pie',
-        'The Pie And The Glory',
-        'Pie Hunting',
-        'The Pie Says',
-      ]
-    },
-    ];
+      "Pie Is Love, Pie Is Life",
+      "One Thousand Pies",
+      "Libation Pie",
+      "Half The Pie",
+      "What The Pie Says Goes",
+      "Swords Into Pies",
+      "Worship The Pie",
+      "The Pie And The Glory",
+      "Pie Hunting",
+      "The Pie Says"
+    ]
+  }
+];
 
 // function to print the different merch products to the merch page.
-  const productBuilder = (productsArray) => {
-      let productDomString = '';
-       for (let i = 0; i < productsArray.length; i++) {
-        let product = productsArray[i];
-         productDomString += `
+const productBuilder = productsArray => {
+  let productDomString = "";
+  for (let i = 0; i < productsArray.length; i++) {
+    let product = productsArray[i];
+    productDomString += `
          <div class="individualProductCard">
           <div class="product-card text-center">
            <h1 class="productCardTitle">${product.name}</h1>
@@ -190,19 +213,18 @@ const products = [
               </footer>
           </div>
          </div>
-        `
-        };
-      
-        printToDOM(productDomString, 'product-card');
-  };
- // productBuilder(products);
+        `;
+  }
 
-  
-  const discCardPrinter = (albumArr) => {
-    let albumDomString = '';
-    for (let i = 0; i < albumArr.length; i++) {
-        const album = albumArr[i]
-        albumDomString += `
+  printToDOM(productDomString, "product-card");
+};
+// productBuilder(products);
+
+const discCardPrinter = albumArr => {
+  let albumDomString = "";
+  for (let i = 0; i < albumArr.length; i++) {
+    const album = albumArr[i];
+    albumDomString += `
             <div class="card text-center" style="width: 22rem;">
                 <img style="width: 100%; height: auto;" src=${
                   album.imageUrl
@@ -213,18 +235,18 @@ const products = [
                    <a href="merch.html" class="btn btn-primary">Purchase</a>
                 </div>
             </div>
-        `
-    }
-    printToDOM(albumDomString, 'disc-cards-div');
-  };
+        `;
+  }
+  printToDOM(albumDomString, "disc-cards-div");
+};
 // discCardPrinter(albums);
 
- //Function that filters info from Upcoming Concerts Array and prints into collapsable list
- const eventDetails = (eventsArray) => {
-  let eventDomString = '';
-  for (i=0; i < eventsArray.length; i++) {
-      let event = eventsArray[i]
-      eventDomString += `
+//Function that filters info from Upcoming Concerts Array and prints into collapsable list
+const eventDetails = eventsArray => {
+  let eventDomString = "";
+  for (i = 0; i < eventsArray.length; i++) {
+    let event = eventsArray[i];
+    eventDomString += `
       <div class="accordion" id="accordionExample">
           <div class="card text-center">
               <div class="card-header" id="headingOne">
@@ -245,60 +267,55 @@ const products = [
               </div>
           </div>
       </div>
-      `
+      `;
   }
-  printToDOM(eventDomString, 'concert-zone');
+  printToDOM(eventDomString, "concert-zone");
 };
 
 // eventDetails(tourDates);
 // init function will allow us to run each function on it's specific page.
 const init = () => {
-  if (document.URL.includes('disc')) {
+  if (document.URL.includes("disc")) {
     discCardPrinter(albums);
-  } else if (document.URL.includes('merch')) {
+  } else if (document.URL.includes("merch")) {
     productBuilder(products);
-  } else if (document.URL.includes('events')) {
+  } else if (document.URL.includes("events")) {
     eventDetails(tourDates);
-  } 
-  if (document.URL.includes('index')) {
+  }
+  if (document.URL.includes("index")) {
     imageBuilder(images);
   }
 };
 
 init();
 
-const sortClick = (event) => {
-  const type = event.target.id
+const sortClick = event => {
+  const type = event.target.id;
   selectedProducts = [];
   for (let i = 0; i < products.length; i++) {
     const product = products[i];
-     if (product.type === type) {
-       selectedProducts.push(product);
-     };
-  };
-
-  if (type === 'all') {
-    productBuilder(products);
-    } else {
-    productBuilder(selectedProducts)
-    };
-
-}; 
-
-
-
-document.getElementById('product-card').addEventListener('click', function(event) {
-  if (event.target.id === 'buy' ) {
-      console.log(event.target.id);
-      window.location.href='./purchase.html';
+    if (product.type === type) {
+      selectedProducts.push(product);
+    }
   }
-}
-);
 
+  if (type === "all") {
+    productBuilder(products);
+  } else {
+    productBuilder(selectedProducts);
+  }
+};
 
-document.getElementById('all').addEventListener('click', sortClick);
-document.getElementById('cassettes').addEventListener('click', sortClick);
-document.getElementById('poster').addEventListener('click', sortClick);
-document.getElementById('sticker').addEventListener('click', sortClick);
+document
+  .getElementById("product-card")
+  .addEventListener("click", function(event) {
+    if (event.target.id === "buy") {
+      console.log(event.target.id);
+      window.location.href = "./purchase.html";
+    }
+  });
 
-
+document.getElementById("all").addEventListener("click", sortClick);
+document.getElementById("cassettes").addEventListener("click", sortClick);
+document.getElementById("poster").addEventListener("click", sortClick);
+document.getElementById("sticker").addEventListener("click", sortClick);
